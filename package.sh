7d90f33d1133b440e20c9112cf3435c8e79258b7
Arjan Molenaar <gaphor@gmail.com>
2011-02-06 21:26:38 +0100

    App loads.

--- package.sh
+++ package.sh
@@ -2,13 +2,14 @@ PYVER=2.6
 INSTALLDIR=Gaphor.app/Contents
 LIBDIR=$INSTALLDIR/lib
 
-mkdir -p $INSTALLDIR
+LOCALDIR=/usr/local
 
 virtualenv --python=python$PYVER --no-site-packages $INSTALLDIR
 
 $INSTALLDIR/bin/easy_install gaphor
 
-#virtualenv --relocatable $INSTALLDIR
+# Make hashbang for python scripts in bin/ relative (#!/usr/bin/env python2.6)
+virtualenv -v --relocatable $INSTALLDIR
 
 # Temp. solution
 SITEPACKAGES=$LIBDIR/python$PYVER/site-packages
@@ -25,8 +26,14 @@ cp -r $oldsite/cairo $SITEPACKAGES
 cp -r $oldsite/gtk-2.0 $SITEPACKAGES
 cp $oldsite/pygtk.pth $SITEPACKAGES
 
+# Copy extra files:
+for dir in etc/pango lib/pango etc/gtk-2.0 lib/gtk-2.0 share/themes; do
+  mkdir -p $INSTALLDIR/$dir
+  cp -r $LOCALDIR/$dir/* $INSTALLDIR/$dir
+done
+
 # Somehow files are writen with mode 444
-find $LIBDIR -type f -exec chmod u+w {} \;
+find $INSTALLDIR -type f -exec chmod u+w {} \;
 
 function log() {
   echo $* >&2
@@ -35,11 +42,9 @@ function log() {
 function resolve_deps() {
   local lib=$1
   local dep
-  otool -L $lib | grep -e '^./usr/local/' |\
+  otool -L $lib | grep -e "^.$LOCALDIR/" |\
       while read dep _; do
-    if test "$dep" != "$lib"; then
-      echo $dep
-    fi
+    echo $dep
   done
 }
 
@@ -53,7 +58,7 @@ function fix_paths() {
   done
 }
 
-binlibs=`find $SITEPACKAGES -type f -name '*.so'`
+binlibs=`find $INSTALLDIR -type f -name '*.so'`
 
 for lib in $binlibs; do
   log Resolving $lib
